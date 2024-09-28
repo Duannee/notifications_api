c954fda7c8b64f717e77ed4daaa8c9bd5c0b6d7d
Duanne Moraes <duanne.souza@gmail.com>
2024-09-28 12:41:37 -0300

    feat: Create notification consumers

--- src/notification/consumers.py
+++ src/notification/consumers.py
@@ -1,8 +1,18 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
 
 
-class NotificationConsumer(WebsocketConsumer):
-    def connect(self):
-        pass
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        self.group_name = f"user_{self.user.id}"
+
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def send_notification(self, event):
+        notification = event["notification"]
+        await self.send(text_data=json.dumps(notification))
