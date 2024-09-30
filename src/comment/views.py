9a85f481de127ff0d23c4d87422e234bc56cabd1
Duanne Moraes <duanne.souza@gmail.com>
2024-09-30 17:37:37 -0300

    feat: Create  reply to comment notification

--- src/comment/views.py
+++ src/comment/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .models import Comment
+from .serializers import CommentSerializer
+from post.models import Post
+from notification.utils import create_notifications
 
-# Create your views here.
+
+class ReplyToCommentView(CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        parent_comment = Comment.objects.get(id=self.kwargs["comment_id"])
+        reply = serializer.save(
+            user=self.request.user, post=parent_comment.post, parent=parent_comment
+        )
+
+        if parent_comment.user != self.request.user:
+            create_notifications(
+                user=parent_comment.user,
+                notification_type="reply_comment",
+                title="Reply to your comment",
+                message=f"{self.request.user.username} reply: {reply.content}",
+            )
