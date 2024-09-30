f1820cbc9e9f777861bb9dd8c7ba56a2519a4338
Duanne Moraes <duanne.souza@gmail.com>
2024-09-30 17:24:06 -0300

    feat: Create notification new like on a post

--- src/like/views.py
+++ src/like/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .models import Like
+from .serializers import LikeSerializer
+from post.models import Post
+from notification.utils import create_notifications
 
-# Create your views here.
+
+class CreateLike(CreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(self.kwarg["post_id"])
+        like = serializer.save(user=self.request.user, post=post)
+
+        if post.author != self.request.user:
+            create_notifications(
+                user=post.author,
+                notification_type="like_post",
+                title="Your post received a like ",
+                message=f"{self.request.user.username} liked your post",
+            )
