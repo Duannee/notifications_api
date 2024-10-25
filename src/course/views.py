1ccc087e0d176027ca0b5875b0175abfbf2c7a07
Duanne Moraes <duanne.souza@gmail.com>
2024-10-25 14:13:00 -0300

    feat: Create course view

--- src/course/views.py
+++ src/course/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Course
+from .serializers import CourseSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class CourseListCreateView(ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(
+            {"message": "Course created successfully", "data": response.data},
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class CourseRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return Response(
+            {"message": "Course updated successfully", "data": response.data},
+            status=status.HTTP_200_OK,
+        )
