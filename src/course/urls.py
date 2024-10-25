e7f6cd9a314a22042bc7270453be352f8bee90b2
Duanne Moraes <duanne.souza@gmail.com>
2024-10-25 14:14:34 -0300

    feat: Create course url

---
+++ src/course/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import CourseListCreateView, CourseRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path("", CourseListCreateView.as_view(), name="course-list-create"),
+    path(
+        "<int:pk>/",
+        CourseRetrieveUpdateDestroyView.as_view(),
+        name="course-retrieve-update-destroy",
+    ),
+]
