1d27e25123c428b9c365ed7e88408dc3c618c504
Duanne Moraes <duanne.souza@gmail.com>
2024-10-28 16:32:55 -0300

    feat: Create event model and make migrations

--- src/event/models.py
+++ src/event/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    organizer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="event_organizer"
+    )
+    participants = models.ManyToManyField(
+        User, related_name="event_participant", blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
