9c6cf8634e62025d9fc87e2b8f609c8a4b938f44
Duanne Moraes <duanne.souza@gmail.com>
2024-11-10 12:45:45 -0300

    refactor: Change event model, take start time and end time

--- src/event/models.py
+++ src/event/models.py
@@ -4,12 +4,14 @@ from django.contrib.auth.models import User
 
 class Event(models.Model):
     organizer = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="event_organizer"
+        User,
+        on_delete=models.CASCADE,
+        related_name="event_organizer",
+        blank=True,
+        null=True,
     )
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
