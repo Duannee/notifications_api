5a14235e6020e82cc98740dd46f110a2a939ac4b
Duanne Moraes <duanne.souza@gmail.com>
2024-11-10 12:22:15 -0300

    refactor: Change event model, take participants table

--- src/event/models.py
+++ src/event/models.py
@@ -3,16 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Event(models.Model):
+    organizer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="event_organizer"
+    )
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    organizer = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="event_organizer"
-    )
-    participants = models.ManyToManyField(
-        User, related_name="event_participant", blank=True
-    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
