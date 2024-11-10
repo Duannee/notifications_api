8dc515799067a5e60210e687b3131ceaa59913ae
Duanne Moraes <duanne.souza@gmail.com>
2024-11-10 12:54:59 -0300

    refactor: Change event serializer, take start time and end time

--- src/event/serializers.py
+++ src/event/serializers.py
@@ -9,10 +9,8 @@ class EventSerializer(serializers.ModelSerializer):
             "id",
             "title",
             "description",
-            "start_time",
-            "end_time",
             "organizer",
             "created_at",
             "updated_at",
         ]
-        read_only_fields = ["created_at", "updated_at", "start_time", "end_time"]
+        read_only_fields = ["created_at", "updated_at"]
