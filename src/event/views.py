8d49af2d37ea4fb54f0ca02d7f2fbb88c4eda424
Duanne Moraes <duanne.souza@gmail.com>
2024-11-19 17:28:31 -0300

    refactor: Put extend schema with tags for documentation

--- src/event/views.py
+++ src/event/views.py
@@ -5,8 +5,10 @@ from .serializers import EventSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(tags=["Event"])
 class EventCreateView(CreateAPIView):
     permission_classes = [IsAuthenticated]
 
@@ -21,6 +23,7 @@ class EventCreateView(CreateAPIView):
         )
 
 
+@extend_schema(tags=["Event"])
 class EventUpdateView(UpdateAPIView):
     permission_classes = [IsAuthenticated]
 
