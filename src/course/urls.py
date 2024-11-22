41c4db20c65286c909f3e63ac896db3b1678c1be
Duanne Moraes <duanne.souza@gmail.com>
2024-11-22 14:43:04 -0300

    refactor: Change url name

--- src/course/urls.py
+++ src/course/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import CourseCreateView, CourseUpdateView
 
 urlpatterns = [
-    path("course/create/", CourseCreateView.as_view(), name="course-list-create"),
+    path("course/create/", CourseCreateView.as_view(), name="course-create"),
     path(
         "course/<int:pk>/update/",
         CourseUpdateView.as_view(),
-        name="course-retrieve-update-destroy",
+        name="course-update",
     ),
 ]
